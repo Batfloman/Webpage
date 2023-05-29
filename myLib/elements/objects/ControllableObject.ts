5a821bf2f2413923d3c18cbe9b61856b2726a170
Florian Hirche <79253255+Batfloman@users.noreply.github.com>
2023-05-29 23:04:38 +0200

    deleted physic folder

--- myLib/elements/objects/ControllableObject.ts
+++ myLib/elements/objects/ControllableObject.ts
@@ -1,5 +1,4 @@
 import { GameObject } from "./GameObject";
 
 export class ControllableObject extends GameObject {
-
 }
--- pages/game/src/shapes/main.ts
+++ pages/game/src/shapes/main.ts
@@ -4,9 +4,3 @@ import { Game } from "../../../../myLib/systems/Game";
 const canvas = document.querySelector("canvas");
 
 const game = new Game(canvas, true);
-
-// const clazz = Util.object.getSuperClassName(Game);
-// console.log(clazz)
-
-console.log(Util.class.getAllSuperClassNames(Game))
-// game.addObject()
